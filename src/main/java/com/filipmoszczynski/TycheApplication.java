15f55e1b395949f4f162999e0711f7e043e87ab2
Filip M <filip.moszczynski@onet.pl>
2020-11-14 10:21:21 +0100

    D6.0 - pom.xml update

--- src/main/java/com/filipmoszczynski/TycheApplication.java
+++ src/main/java/com/filipmoszczynski/TycheApplication.java
@@ -1,4 +1,4 @@
-package com.michalszalkowski;
+package com.filipmoszczynski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
