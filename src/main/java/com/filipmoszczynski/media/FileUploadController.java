008c474d1e537da2ee4de6ca69524f4088d025d7
Filip M <filip.moszczynski@onet.pl>
2020-11-14 14:23:01 +0100

    D6.0 - FileUploadController update

--- src/main/java/com/filipmoszczynski/media/FileUploadController.java
+++ src/main/java/com/filipmoszczynski/media/FileUploadController.java
@@ -3,6 +3,7 @@ package com.filipmoszczynski.media;
 import com.filipmoszczynski.media.storage.FileSystemStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,8 @@ public class FileUploadController {
     }
 
     @GetMapping("/media")
-    public String getMediaPage() {
+    public String getMediaPage(Model model) {
+        model.addAttribute("files", storage.list());
         return "media/index.html";
     }
 
