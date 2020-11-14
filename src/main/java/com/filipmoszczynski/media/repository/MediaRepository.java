a61c27ccf0145b3b56c4e0eb9393522e2a13ce1a
Filip M <filip.moszczynski@onet.pl>
2020-11-14 12:35:47 +0100

    D6.0 - MediaRepository

---
+++ src/main/java/com/filipmoszczynski/media/repository/MediaRepository.java
@@ -0,0 +1,8 @@
+package com.filipmoszczynski.media.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+public interface MediaRepository
+        extends JpaRepository<MediaEntity, Long>,
+        JpaSpecificationExecutor<MediaEntity> {
+}
